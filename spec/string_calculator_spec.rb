3220882a6fa4690309b1598e07ab274aa3a6e61d
avayath-tech <avayath.tech@outlook.com>
2025-10-22 18:15:05 +0530

    Step 2: Added support for unknown number of comma's

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe StringCalculator do
   it { expect(sc.add("")).to eq 0 }          # "" == 0
   it { expect(sc.add("1")).to eq 1 }          # "1" == 1
   it { expect(sc.add("1,2")).to eq 3 }        # "1,2" == 3
+  it { expect(sc.add("5,7,10,3")).to eq 25 }  # to allow unknown number of comma's
+
 end
