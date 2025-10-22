01b91c66fca55f6e0922d58210f4baed6e75cb0c
avayath-tech <avayath.tech@outlook.com>
2025-10-22 18:25:19 +0530

    Step 5: raise error for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,10 +9,15 @@ class StringCalculator
 
     if input.start_with?("//")
       header, body = input.split("\n", 2)
-      delims = [header[2]]   # single-char delimiter
+      delims = [header[2]]
     end
 
     nums = body.split(Regexp.union(delims)).map!(&:to_i)
+
+    if (neg = nums.find { |n| n < 0 })
+      raise ArgumentError, "negatives not allowed: #{neg}"
+    end
+
     nums.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe StringCalculator do
   it { expect(sc.add("5,7,10,3")).to eq 25 }  # to allow unknown number of comma's
   it { expect(sc.add("1\n2,3")).to eq 6 }     # to add support for
   it { expect(sc.add("//;\n1;2")).to eq 3 }   # to add support for delimiter
+
+  it "raises on a negative number" do
+    expect { sc.add("1,-2,3") }.to raise_error(ArgumentError, /negatives not allowed/i)
+  end
+
 end
